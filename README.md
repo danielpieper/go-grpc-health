3a32f7d2f0f22518833d6ebfe488791853324097
Daniel Pieper <daniel@daniel-pieper.com>
2021-02-14 18:07:43 +0100

    Add internal test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+![Go](https://github.com/danielpieper/health/workflows/Go/badge.svg?branch=main)
+
 # health
 Simple health checks following the GRPC Health Checking Protocol
---
+++ server_internal_test.go
@@ -0,0 +1,35 @@
+package health
+
+import (
+	"testing"
+)
+
+func TestAddHealthCheck(t *testing.T) {
+  const service = "grpc.test.service"
+
+  healthServer := NewServer()
+
+  h := fakeHealthCheck{result: true}
+  healthServer.AddHealthCheck(service, h)
+
+  actual, ok := healthServer.checks[service]
+  if !ok {
+    t.Errorf("expected '%s', got nil", service)
+  }
+
+  if len(actual) == 0 {
+    t.Error("expected HealthCheck, got 0")
+  }
+
+  if !actual[0].IsHealthy() {
+    t.Error("expected IsHealthy = true, got false")
+  }
+}
+
+type fakeHealthCheck struct {
+  result bool
+}
+
+func (h fakeHealthCheck) IsHealthy() bool {
+  return h.result
+}
