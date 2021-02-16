1dc516959da5259ac1e1747bfd42598986942936
Daniel Pieper <daniel@daniel-pieper.com>
2021-02-16 20:18:28 +0100

    Fix test

--- grpc_health_test.go
+++ grpc_health_test.go
@@ -4,12 +4,12 @@ import (
 	"testing"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/health"
+  "github.com/danielpieper/go-grpc-health"
 	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func TestRegister(t *testing.T) {
 	s := grpc.NewServer()
-	healthgrpc.RegisterHealthServer(s, health.NewServer())
+	healthgrpc.RegisterHealthServer(s, grpc_health.NewServer())
 	s.Stop()
 }
